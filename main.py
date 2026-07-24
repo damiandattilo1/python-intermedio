a34a4be0d022276d6dab81921eff87984fc2dc81
Damian Dattilo <damiandattilo@gmail.com>
2026-07-24 19:33:18 -0300

    configurar logging basico en main.py para que los logs se muestren en consola

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import logging
 import vista
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 if __name__ == "__main__":
     vista.crear_interfaz()
